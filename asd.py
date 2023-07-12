374a0098d034d9f7e7917b4e81b2d4b59ed6cabc
Liza176 <138390166+Liza176@users.noreply.github.com>
2023-07-12 15:59:33 +0600

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,85 @@
+# [1,2,3,4,5]
+# ot = int(input('от '))
+# do = int(input('до '))
+# mas = []
+# which = input('Четные или Нечетные ')
+
+# for t in range(ot,do):
+#     if which == 'Четные':
+#         if t % 2 == 0:
+#              mas.append(t)
+
+#     else:
+#         if t % 2 != 0:
+#              mas.append(t)
+
+
+# print(mas)
+  
+
+# n = int(input('Напишите сколько раз '))
+# mas = []
+
+# for i in range(n):
+#     name = (input('Напишите имя '))
+#     fam = (input('Напишите фамилию '))
+#     mas.append(name)
+#     mas.append(fam)
+    
+# print(mas)
+
+# n = int(input('Напишите сколько раз '))
+# mas = []
+
+# for i in range(n):
+#     name = int(input('Ваше число '))
+#     mas.append(name)
+
+# print(mas)
+# max = -99999
+# min = 99999
+
+# for u in mas:
+#     if u > max :
+#         max = u
+#     if u < min :
+#         min = u
+
+# sred = 0
+
+# for w in mas:
+#     if w == max:
+#         continue
+#     elif w == min:
+#         continue
+#     else:
+#         sred = w
+
+# print(sred)
+
+
+# n = int(input('Напишите сколько раз '))
+# mas = []
+
+# for i in range(n):
+#     name = (input('Напишите имя '))
+#     fam = (input('Напишите фамилию '))
+#     mas.append(name)
+#     mas.append(fam)
+    
+# print(mas)
+
+# del_ = input('Какое имя/фамилию вы хотите удалить ')
+# index = 0
+
+# for r in mas :
+#     if r == del_:
+#         break
+#     index += 1   
+
+# del mas[index]
+# print(mas)
+# Привет как дела
+
+# ["Привет","как","дела"]
+
